0a140879937cc73705909945b4ebc7b1e40d6ca9
César Augusto <cesaraugustokg6@hotmail.com>
2018-02-09 04:49:29 -0200

    Primeiro commit

---
+++ app/Signature.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Signature extends Model {
+
+    protected $fillable = ['name', 'email', 'body', 'flagged_at'];
+
+    public function scopeIgnoreFlagged($query) {
+        return $query->where('flagged_at', null);
+    }
+
+    public function getAvatarAttribute() {
+        return sprintf('https://www.gravatar.com/avatar/%s?s=100', md5($this->email));
+    }
+
+}
